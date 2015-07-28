7994595e6b8a562c8c545f2cb9079345fe6aa910
Andrew Smith <emertechie@gmail.com>
2015-07-28 14:56:22 +0700

    Add test version of hotrod-job-es-index-clean (whatIf flag enabled)

--- src/api/jobs/configureJobs.js
+++ src/api/jobs/configureJobs.js
@@ -2,10 +2,21 @@
 
 var config = require('../../config');
 var jobs = require('hotrod-jobs');
-var serverStatsJob = require('./serverStatsJob')();
 
 var serverUpdateIntervalSeconds = parseInt(config.getRequired('SERVER_UPDATE_INTERVAL_SECS'));
+var indexCleanupIntervalSeconds = parseInt(config.getRequired('ES_INDEX_CLEANUP_INTERVAL_SECS'));
+var esHost = config.getRequired('ELASTICSEARCH');
+
+var cleanupLogstashIndices = require('hotrod-job-es-index-cleanup')(esHost, {
+    index: 'serverstats-*',
+    skip: 30,
+    action: 'close',
+    whatIf: true
+});
+
+var serverStatsJob = require('./serverStatsJob')();
 
 module.exports = function() {
     jobs.runContinuous(serverStatsJob, serverUpdateIntervalSeconds);
+    jobs.runContinuous(cleanupLogstashIndices, indexCleanupIntervalSeconds);
 };
