5c846f61c4e3cc3e95663c1e4b69741b4c9c7517
Stephan Buys <stephan.buys@panoptix.co.za>
2015-07-31 14:22:03 +0200

    WIP: configurations needed to have serverstats cleaned automatically

--- src/api/jobs/configureJobs.js
+++ src/api/jobs/configureJobs.js
@@ -2,16 +2,25 @@
 
 var config = require('../../config');
 var jobs = require('hotrod-jobs');
+var moment = require('moment');
 
 var serverUpdateIntervalSeconds = parseInt(config.getRequired('SERVER_UPDATE_INTERVAL_SECS'));
 var indexCleanupIntervalSeconds = parseInt(config.getRequired('ES_INDEX_CLEANUP_INTERVAL_SECS'));
 var esHost = config.getRequired('ELASTICSEARCH');
 
+
+var compareFunc = function(date1, date2) {
+    var d1 = moment(date1.split("serverstats-")[1],"YYYY-MM-DD");
+    var d2 = moment(date2.split("serverstats-")[1],"YYYY-MM-DD");
+    return (d1 < d2) ? -1 : (d2 > d1) ? 1 : 0;
+};
+
 var cleanupLogstashIndices = require('hotrod-job-es-index-cleanup')(esHost, {
     index: 'serverstats-*',
-    skip: 30,
+    skip: config.getRequired('ES_INDEX_CLEANUP_SPAN'),
+    indexNameCompare: compareFunc,
     action: 'delete',
-    whatIf: true
+    whatIf: config.getRequired('ES_INDEX_CLEANUP_WHATIF')
 });
 
 var serverStatsJob = require('./serverStatsJob')();
