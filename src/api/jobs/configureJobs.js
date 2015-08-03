87609197f9499349ad7dbc00fd192dfb7ab6c077
Stephan Buys <stephan.buys@panoptix.co.za>
2015-08-03 14:08:26 +0200

    Add the two default jobs, just disable 'whatIf' to make it go

--- src/api/jobs/configureJobs.js
+++ src/api/jobs/configureJobs.js
@@ -5,20 +5,33 @@ var jobs = require('hotrod-jobs');
 var moment = require('moment');
 var esClient = require('../elasticsearch');
 
-var serverUpdateIntervalSeconds = parseInt(config.getRequired('SERVER_UPDATE_INTERVAL_SECS'));
-var indexCleanupIntervalSeconds = parseInt(config.getRequired('ES_INDEX_CLEANUP_INTERVAL_SECS'));
+var serverStatsUpdateIntervalSeconds = parseInt(config.getRequired('SS_INDEX_UPDATE_INTERVAL_SECS'));
 
-var cleanupLogstashIndices = require('hotrod-job-es-index-cleanup')({
+var serverStatsIndexCleanupIntervalSeconds = parseInt(config.getRequired('SS_INDEX_CLEANUP_INTERVAL_SECS'));
+var logstashIndexCleanupIntervalSeconds = parseInt(config.getRequired('LS_INDEX_CLEANUP_INTERVAL_SECS'));
+
+var cleanupServerStatsIndices = require('hotrod-job-es-index-cleanup')({
+    label: 'delete-serverstats',
     esClient: esClient,
     index: 'serverstats-*',
-    skip: parseInt(config.getRequired('ES_INDEX_CLEANUP_SPAN')),
+    skip: parseInt(config.getRequired('SS_INDEX_CLEANUP_SPAN')),
+    action: 'delete',
+    whatIf: config.getRequired('SS_INDEX_CLEANUP_WHATIF')
+});
+
+var closeLogstashIndices = require('hotrod-job-es-index-cleanup')({
+    label: 'close-logstash',
+    esClient: esClient,
+    index: 'logstash-*',
+    skip: parseInt(config.getRequired('LS_INDEX_CLEANUP_SPAN')),
     action: 'close',
-    whatIf: config.getRequired('ES_INDEX_CLEANUP_WHATIF')
+    whatIf: config.getRequired('LS_INDEX_CLEANUP_WHATIF')
 });
 
 var serverStatsJob = require('./serverStatsJob')();
 
 module.exports = function() {
-    jobs.runContinuous(serverStatsJob, serverUpdateIntervalSeconds);
-    jobs.runContinuous(cleanupLogstashIndices, indexCleanupIntervalSeconds);
+    jobs.runContinuous(serverStatsJob, serverStatsUpdateIntervalSeconds);
+    jobs.runContinuous(cleanupServerStatsIndices, serverStatsIndexCleanupIntervalSeconds);
+    jobs.runContinuous(closeLogstashIndices, logstashIndexCleanupIntervalSeconds);
 };
